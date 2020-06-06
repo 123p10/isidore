0270283380619987a25e4cedc7b6592e48c3182c
123p10 <obrake3@gmail.com>
2020-06-06 17:30:00 -0400

    Add Path Simplification

--- runtime/runtime.cpp
+++ runtime/runtime.cpp
@@ -4,6 +4,6 @@ void Runtime::createPrototypes(){
 	std::vector<Argument> GC_MALLOC_TYPES;
 	GC_MALLOC_TYPES.push_back({llvm::IntegerType::get(*(code_gen->TheContext),32),"size"});
 	std::unique_ptr<PrototypeAST> GC_PROTO_AST = std::make_unique<PrototypeAST>("GC_malloc", GC_MALLOC_TYPES, code_gen, voidPtr);
-	auto *GCIR = GC_PROTO_AST->codegen();
+	GC_PROTO_AST->codegen();
 	(*(code_gen->FunctionProtos))[GC_PROTO_AST->getName()] = std::move(GC_PROTO_AST);
 }
